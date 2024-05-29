6fe8602074e444034551f055769705a0b8d27cbf
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-05-29 13:26:22 +0700

    update mua

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Bamboo.Mua.MixProject do
   use Mix.Project
 
-  @version "0.1.5"
+  @version "0.2.0"
   @repo_url "https://github.com/ruslandoga/bamboo_mua"
 
   def project do
@@ -45,7 +45,7 @@ defmodule Bamboo.Mua.MixProject do
     [
       {:bamboo, "~> 2.0"},
       {:mail, "~> 0.3.0"},
-      {:mua, "~> 0.1.5"},
+      {:mua, "~> 0.2.0"},
       {:castore, "~> 0.1.0 or ~> 1.0", only: [:dev, :test]},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
       {:req, "~> 0.4.8", only: :test},
